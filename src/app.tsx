2a793523bea12cb4104258607397bb4517cc9315
ottogugel <harpgamer31@gmail.com>
2024-02-22 13:45:05 -0300

    add radix dialog

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,22 @@
-import { FileDown, Filter, MoreHorizontal, Plus, Search } from 'lucide-react'
+import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import * as Dialog from "@radix-ui/react-dialog";
 import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
 
-import { Header } from './components/header';
-import { Tabs } from './components/tabs';
-import { Button } from './components/ui/button';
-import { Control, Input } from './components/ui/input';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
-import { Pagination } from './components/pagination';
-
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
+import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
+import { Pagination } from "./components/pagination";
 
 // JSON -> Typescript
 export interface TagResponse {
@@ -28,7 +35,6 @@ export interface Tag {
   id: string;
 }
 
-
 export function App() {
   const [searchParams, setSearchParams] = useSearchParams();
   const urlFilter = searchParams.get("filter") ?? "";
@@ -37,7 +43,6 @@ export function App() {
 
   // Converter a página em um número se não estarei na página 1.
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
-
 
   // Função que vai me devolver os dados da API. (Requisição GET)
   const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
@@ -58,12 +63,12 @@ export function App() {
   });
 
   function handleFilter() {
-    setSearchParams(params => {
-      params.set('page', '1')
-      params.set('filter', filter)
+    setSearchParams((params) => {
+      params.set("page", "1");
+      params.set("filter", filter);
 
       return params;
-    })
+    });
   }
 
   if (isLoading) {
@@ -79,10 +84,31 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            {/*  Acessibilidade para o modal. */}
+            <Dialog.Portal>
+              {/* Div preta que fica no conteudo */}
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className="space-y-10 fixed right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-900">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create Tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts.
+                  </Dialog.Description>
+                </div>
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </div>
 
         <div className="flex items-center justify-between">
